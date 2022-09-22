ecf075fea92db6814a2cd9a51d34049b7b737870
Ryan O'Connor <ryan.oconnor@gmail.com>
2022-09-22 00:35:52 -0400

    Updating

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,13 @@ import ColumnLayout from '@splunk/react-ui/ColumnLayout';
 import Heading from '@splunk/react-ui/Heading';
 import dynamic from 'next/dynamic';
 import React, { useState } from 'react';
+import User from '@splunk/react-icons/User';
 
 const File = dynamic(() => import('@splunk/react-ui/File'), {
+    ssr: false,
+});
+
+const Text = dynamic(() => import('@splunk/react-ui/Text'), {
     ssr: false,
 });
 
@@ -16,7 +21,21 @@ const colStyle = {
 export default function Home() {
     const [filesArray, setFiles] = useState([]);
     const [children, setChildren] = useState(<></>);
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
 
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const updateUsername = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+    };
     function loadFile(file) {
         const fileItem = { name: file.name };
 
@@ -31,7 +50,6 @@ export default function Home() {
         ));
 
         setChildren(children_local);
-
         return fileItem;
     }
 
@@ -39,7 +57,6 @@ export default function Home() {
         const newItems = files.map(loadFile);
 
         setFiles([...filesArray, ...newItems]);
-        console.log(files);
     };
 
     const handleRemoveFile = ({ index }) => {
@@ -59,6 +76,42 @@ export default function Home() {
                     <ColumnLayout.Column style={colStyle} span={10}></ColumnLayout.Column>
                 </ColumnLayout.Row>
             </ColumnLayout>
+            <div style={{ width: '100%', display: 'block' }}>
+                <div style={{ margin: 'auto', textAlign: 'center' }}>
+                    <Heading level={2} style={{ margin: 'auto', textAlign: 'center' }}>
+                        Enter Your Username and Password for Splunk.com
+                    </Heading>
+                    <br />
+                    <form onsubmit={(e) => submitForm(e)}>
+                        <Text
+                            defaultValue=""
+                            startAdornment={
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        padding: '0 8px',
+                                    }}
+                                >
+                                    <User size={1} />
+                                </div>
+                            }
+                            value={username}
+                            onChange={(e) => updateUsername(e)}
+                            inline
+                            placeholder="Username"
+                        />
+                        <Text
+                            inline
+                            type="password"
+                            value={password}
+                            onChange={(e) => updatePassword(e)}
+                        />
+                        <input type="submit" />
+                    </form>
+                </div>
+            </div>
+            <br />
             <File onRequestAdd={handleAddFiles} onRequestRemove={handleRemoveFile} allowMultiple>
                 {children}
             </File>{' '}
