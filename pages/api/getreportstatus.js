fa094fa36753d35361839aa2b06f699bb66c8891
Ryan O'Connor <ryan.oconnor@gmail.com>
2022-09-23 11:25:08 -0400

    Update getreportstatus.js

--- pages/api/getreportstatus.js
+++ pages/api/getreportstatus.js
@@ -1,25 +1,21 @@
 export default function handler(req, res) {
-  fetch(
-    "https://appinspect.splunk.com/v1/app/validate/status/" +
-      req.body.request_id,
-    {
-      method: "GET",
-      headers: {
-        Authorization: "bearer " + req.body.token,
-      },
-    }
-  )
-    .then(async (res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      var data = await res.json();
-      throw { data: data, status: response.status };
+    fetch('https://appinspect.splunk.com/v1/app/validate/status/' + req.body.request_id, {
+        method: 'GET',
+        headers: {
+            Authorization: 'bearer ' + req.body.token,
+        },
     })
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((response) => {
-      res.status(response.status).json(response.data);
-    });
+        .then(async (res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            var data = await res.json();
+            throw { data: data, status: response.status };
+        })
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((response) => {
+            res.status(response.status).json(response.data);
+        });
 }
