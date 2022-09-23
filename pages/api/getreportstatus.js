733aa6b4a3bd61b9bf386a37c7d6b3bbf6c6ac29
Steve McMaster <smcmaster@splunk.com>
2022-09-23 18:45:32 -0400

    fix

--- pages/api/getreportstatus.js
+++ pages/api/getreportstatus.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   fetch(
-    "https://appinspect.splunk.com/v1/app/validate/status/" +
+    'https://appinspect.splunk.com/v1/app/validate/status/' +
       req.body.request_id,
     {
-      method: "GET",
+      method: 'GET',
       headers: {
-        Authorization: "bearer " + req.body.token,
+        Authorization: 'bearer ' + req.body.token,
       },
     }
   )
@@ -16,26 +16,10 @@ export default function handler(req, res) {
       var data = await response.json();
       throw { data: data, status: response.status };
     })
-<<<<<<< Updated upstream
-        .then(async (res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            var data = await res.json();
-            throw { data: data, status: res.status };
-        })
-        .then((data) => {
-            res.status(200).json(data);
-        })
-        .catch((response) => {
-            res.status(response.status).json(response.data);
-        });
-=======
     .then((data) => {
       res.status(200).json(data);
     })
     .catch((response) => {
       res.status(response.status).json(response.data);
     });
->>>>>>> Stashed changes
 }
