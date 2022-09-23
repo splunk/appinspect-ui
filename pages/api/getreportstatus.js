43473cf8d56b5151fe5b21b0ef64a0a24c3785a6
Ryan O'Connor <ryan.oconnor@gmail.com>
2022-09-22 21:55:14 -0400

    Updating APIs to have proper error handling

--- pages/api/getreportstatus.js
+++ pages/api/getreportstatus.js
@@ -13,5 +13,8 @@ export default function handler(req, res) {
         })
         .then((json) => {
             res.status(200).json(json);
+        })
+        .catch((response) => {
+            res.status(response.status).json(response.json());
         });
 }
